a427f62aaa2ae61030b520107c6ec92ca4e603b1
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-02-20 22:41:22 +1030

    Add local/remote user instantiation

---
+++ Assets/Scripts/MyPlayerControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MyPlayerControl : MonoBehaviourPunCallbacks
+{
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject localPlayerPrefab;
+    public GameObject remotePlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate(this.remotePlayerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+        // PhotonNetwork.InstantiateSceneObject()
+    }
+
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+            // LoadArena();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+            // LoadArena();
+        }
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log("Leave the room OnDestroy");
+        // PhotonNetwork.LeaveRoom();
+    }
+
+    #region Photon Callbacks
+    /// <summary>
+    /// Called when the local player left the room. We need to load the launcher scene.
+    /// </summary>
+    public override void OnLeftRoom()
+    {
+        print("Leave the room!");
+    }
+    #endregion
+}
