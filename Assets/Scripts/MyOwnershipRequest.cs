8d3f5357bee19fc7f71df5ab2700df99df34fd4e
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-02-26 12:01:23 +1030

    Get Ownership of the sceneobject

---
+++ Assets/Scripts/MyOwnershipRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MyOwnershipRequest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var pv = GetComponent<PhotonView>();
+        pv.RequestOwnership();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
