ee4e44779649b4110d95df12bbcbb976df9e2306
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-03-08 23:39:53 +1030

    Fix the issue that quaternion not normalized

---
+++ Assets/Scripts/MySceneObjectController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MySceneObjectController : MonoBehaviourPunCallbacks, IPunObservable
+{
+    private GameObject tracker;
+    private Vector3 position;
+    private Quaternion rotation;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            position = rb.position;
+            rotation = rb.rotation;
+        }
+        else
+        {
+            rb.MovePosition(position);
+            rb.MoveRotation(rotation.normalized);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(position);
+            stream.SendNext(rotation);
+        }
+        else
+        {
+            position = (Vector3)stream.ReceiveNext();
+            rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
