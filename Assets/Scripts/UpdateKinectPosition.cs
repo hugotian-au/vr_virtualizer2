48a0fd494803d3b1ab78b24fb64882356d648ea5
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-04-05 16:47:11 +0930

    Add a position updating scripts for cameras

---
+++ Assets/Scripts/UpdateKinectPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateKinectPosition : MonoBehaviour
+{
+    public Vector3 registeredPosition = new Vector3(1.677976f, 2.498745f, 1.319135f);
+    public Quaternion registeredRotatoin = Quaternion.Euler(151.641f, 30.689f, 0.9599919f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = registeredPosition;
+        transform.localRotation = registeredRotatoin;
+    }
+}
