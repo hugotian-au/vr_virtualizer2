514e54f13ed09b8df41e7c1112c9fec5fe436840
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-04-13 11:13:27 +0930

    Add script to init the live content's parent's position

---
+++ Assets/Scripts/MyLiveObjectParentPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLiveObjectParentPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.name == "ak_content_0")
+        {
+            transform.localPosition = new Vector3(-0.08f, 0.04f, 0.0f);
+        }
+        else if (gameObject.name == "ak_content_1")
+        {
+            transform.localPosition = new Vector3(-0.03f, -0.03f, 0.02f);
+        }
+        else if(gameObject.name == "ak_content_2")
+        {
+            transform.localPosition = new Vector3(-0.05f, -0.005f, 0.0f);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
