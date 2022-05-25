04c16340700d6c038f094affc9497e0a24f7bb33
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-05-25 09:32:06 +0930

    Change from public to private for the position of soma blocks

--- Assets/Scripts/MyInitBlockPosition.cs
+++ Assets/Scripts/MyInitBlockPosition.cs
@@ -5,26 +5,26 @@ using Photon.Pun;
 
 public class MyInitBlockPosition : MonoBehaviour
 {
-    public Vector3 blockPosition1 = new Vector3(0.549f, 1.4f, -1.365f);
-    public Quaternion blockRotatoin1 = Quaternion.Euler(-83.369f, 185.838f, -10.511f);
+    private Vector3 blockPosition1 = new Vector3(0.549f, 1.4f, -1.365f);
+    private Quaternion blockRotatoin1 = Quaternion.Euler(-83.369f, 185.838f, -10.511f);
 
-    public Vector3 blockPosition2 = new Vector3(0.897f, 1.3f, -1.365f);
-    public Quaternion blockRotatoin2 = Quaternion.Euler(-90.00001f, 0f, -181.849f);
-    
-    public Vector3 blockPosition3 = new Vector3(1.222f, 1.283f, -1.342f);
-    public Quaternion blockRotatoin3 = Quaternion.Euler(0f, -87.826f, 0f);
+    private Vector3 blockPosition2 = new Vector3(0.897f, 1.3f, -1.365f);
+    private Quaternion blockRotatoin2 = Quaternion.Euler(-90.00001f, 0f, -181.849f);
 
-    public Vector3 blockPosition4 = new Vector3(0.746f, 1.653f, -1.199f);
-    public Quaternion blockRotatoin4 = Quaternion.Euler(0f, 93.69401f, 0f);
+    private Vector3 blockPosition3 = new Vector3(1.222f, 1.283f, -1.342f);
+    private Quaternion blockRotatoin3 = Quaternion.Euler(0f, -87.826f, 0f);
 
-    public Vector3 blockPosition5 = new Vector3(0.94f, 1.64f, -1.191f);
-    public Quaternion blockRotatoin5 = Quaternion.Euler(0f, 0f, -93.97201f);
+    private Vector3 blockPosition4 = new Vector3(0.565f, 1.653f, -1.199f);
+    private Quaternion blockRotatoin4 = Quaternion.Euler(0f, 93.69401f, 0f);
 
-    public Vector3 blockPosition6 = new Vector3(1.231f, 1.636f, -1.221f);
-    public Quaternion blockRotatoin6 = Quaternion.Euler(0f, 92.313f, 0f);
+    private Vector3 blockPosition5 = new Vector3(0.89f, 1.64f, -1.191f);
+    private Quaternion blockRotatoin5 = Quaternion.Euler(0f, 0f, -93.97201f);
 
-    public Vector3 blockPosition7 = new Vector3(0.615f, 0.964f, -1.183f);
-    public Quaternion blockRotatoin7 = Quaternion.Euler(2.963f, 89.75401f, 180.153f);
+    private Vector3 blockPosition6 = new Vector3(1.231f, 1.636f, -1.221f);
+    private Quaternion blockRotatoin6 = Quaternion.Euler(0f, 92.313f, 0f);
+
+    private Vector3 blockPosition7 = new Vector3(0.615f, 0.964f, -1.183f);
+    private Quaternion blockRotatoin7 = Quaternion.Euler(2.963f, 89.75401f, 180.153f);
 
     private bool updated = false;
     // Start is called before the first frame update
