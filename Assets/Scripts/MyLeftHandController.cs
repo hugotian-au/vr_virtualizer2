3e6be38b19afa2a13c00540b5ade2848854e6b40
HugoTian <huayuantian@mymail.unisa.edu.au>
2022-07-02 10:27:29 +0930

    Make hands in AR side following hands in VR side

---
+++ Assets/Scripts/MyLeftHandController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLeftHandController : MonoBehaviour
+{
+    private GameObject leftHandController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftHandController = GameObject.Find("LeftHandAnchor");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftHandController != null)
+        {
+            transform.localPosition = leftHandController.transform.localPosition;
+            transform.localRotation = leftHandController.transform.localRotation;
+        }
+    }
+}
---
+++ Assets/Scripts/MyRightHandController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRightHandController : MonoBehaviour
+{
+    private GameObject rightHandController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightHandController = GameObject.Find("RightHandAnchor");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightHandController != null)
+        {
+            transform.localPosition = rightHandController.transform.localPosition;
+            transform.localRotation = rightHandController.transform.localRotation;
+        }
+    }
+}
